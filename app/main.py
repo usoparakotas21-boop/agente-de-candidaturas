c0461ca9d5e6efe628f7f235aee894a75afc6aab
Paulo Henrique <henriqueoliveirarh93@gmail.com>
2026-09-01 23:19:41 -0300

    feat: streamline onboarding into three guided stages

--- app/main.py
+++ app/main.py
@@ -69,6 +69,7 @@ def _page(path: Path) -> HTMLResponse:
     extra = ""
     if path.name == "vagas.html": extra = '<script src="/static/jobs-enhance.js"></script>'
     if path.name == "candidaturas.html": extra = '<script src="/static/applications-enhance.js"></script><script src="/static/applications-transparency.js"></script>'
+    if path.name == "onboarding.html": extra = '<script src="/static/onboarding-v2.js"></script>'
     if path.name == "profile.html": extra = '<script src="/static/profile-enhance.js"></script>'
     if path.name == "configuracoes.html": extra = '<script src="/static/preferences-enhance.js"></script>'
     if path.name == "configuracoes.html": extra += '<script src="/static/alerts-enhance.js"></script>'
