91f8b0d688d88ac521d2bd08dff13034e4d44faf
Paulo Henrique <henriqueoliveirarh93@gmail.com>
2026-09-01 22:53:52 -0300

    feat: simplificar preferencias em fases

--- app/main.py
+++ app/main.py
@@ -70,6 +70,7 @@ def _page(path: Path) -> HTMLResponse:
     if path.name == "vagas.html": extra = '<script src="/static/jobs-enhance.js"></script>'
     if path.name == "candidaturas.html": extra = '<script src="/static/applications-enhance.js"></script>'
     if path.name == "profile.html": extra = '<script src="/static/profile-enhance.js"></script>'
+    if path.name == "configuracoes.html": extra = '<script src="/static/preferences-enhance.js"></script>'
     html = html.replace("</body>", extra + "</body>", 1)
     return HTMLResponse(html.replace("<body>", "<body>" + nav + crumb, 1))
 
